3d6362a3545568571ba1c597c50c3d38f443186f
Danila Volkov <danila.volkov@noxlovette.com>
2026-05-11 13:24:33 +0300

    build: gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  kit: {}
+  kit: {
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.env.BASE_PATH || ''
+    },
+    prerender: {
+      handleHttpError: 'warn',
+      handleMissingId: 'warn'
+    }
+  }
 };
 
 export default config;
